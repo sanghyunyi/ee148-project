4135e2e29a78f18159e50433dc5ab32f1c226a5d
sanghyunyi <syi@caltech.edu>
2021-04-19 02:02:22 -0700

    better data augmentation

--- train.py
+++ train.py
@@ -107,14 +107,20 @@ class DatasetFolder():
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.Pad(512),
+        transforms.CenterCrop(512),
+        transforms.Resize(224),
+        transforms.RandomRotation(180),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(),
+        transforms.RandomPerspective(),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Pad(512),
+        transforms.CenterCrop(512),
+        transforms.Resize(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
